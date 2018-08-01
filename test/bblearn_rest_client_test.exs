9555eab7c0bfcca019b45dd14f26889c92f7cb96
Mark Kauffman <markkauffman2000@gmail.com>
2018-07-31 17:13:05 -0700

    tests for all current methods. phoneixdsk system role doesnt pull a user. TODO: fix that

--- test/bblearn_rest_client_test.exs
+++ test/bblearn_rest_client_test.exs
@@ -1,5 +1,5 @@
 defmodule BblearnRestClientTest do
-  
+
   use ExUnit.Case
   alias Learn.RestClient
   doctest BblearnRestClient
@@ -8,6 +8,10 @@ defmodule BblearnRestClientTest do
   @app_key Application.get_env(:bblearn_rest_client, :oauth)[:app_key]
   @app_secret Application.get_env(:bblearn_rest_client, :oauth)[:app_secret]
   @learn_server Application.get_env(:bblearn_rest_client, :oauth)[:learn_server]
+
+  setup_all do
+    IO.puts "If you run into errors be certain to have set these environment variables: APP_KEY, APP_SECRET, and LEARN_SERVER."
+  end
 
   test "greets the world" do
     IO.puts "test: greets the world"
@@ -51,5 +55,64 @@ defmodule BblearnRestClientTest do
     assert response.status_code == 404
   end
 
+  test "get_courses" do
+    IO.puts "test: get_courses"
+    rc = RestClient.new(@learn_server, @app_key, @app_secret)
+    assert Map.has_key?(rc, :token) == true
+    rcauth = RestClient.authorize(rc)
+    assert rcauth.token["token_type"] == "bearer"
+    {code, response} = RestClient.get_courses(rcauth)
+    assert code == :ok
+    assert response.status_code == 200
+  end
+
+  test "get_courses_users" do
+    IO.puts "test: get_courses_users"
+    rc = RestClient.new(@learn_server, @app_key, @app_secret)
+    assert Map.has_key?(rc, :token) == true
+    rcauth = RestClient.authorize(rc)
+    assert rcauth.token["token_type"] == "bearer"
+    {code, response} = RestClient.get_courses_users(rcauth, "courseId:1")
+    assert code == :ok
+    assert response.status_code == 200
+  end
+
+  test "get_dataSources" do
+    IO.puts "test: get_dataSources"
+    rc = RestClient.new(@learn_server, @app_key, @app_secret)
+    assert Map.has_key?(rc, :token) == true
+    rcauth = RestClient.authorize(rc)
+    assert rcauth.token["token_type"] == "bearer"
+    {code, response} = RestClient.get_dataSources(rcauth)
+    assert code == :ok
+    assert response.status_code == 200
+  end
+
+  test "get_system_version" do
+    IO.puts "test: get_system_version"
+    rc = RestClient.new(@learn_server, @app_key, @app_secret)
+    assert Map.has_key?(rc, :token) == true
+    rcauth = RestClient.authorize(rc)
+    assert rcauth.token["token_type"] == "bearer"
+    {code, response} = RestClient.get_system_version(rcauth)
+    assert code == :ok
+    assert response.status_code == 200
+  end
+
+  test "get_user" do
+    IO.puts "test: get_user"
+    rc = RestClient.new(@learn_server, @app_key, @app_secret)
+    assert Map.has_key?(rc, :token) == true
+    rcauth = RestClient.authorize(rc)
+    assert rcauth.token["token_type"] == "bearer"
+    {code, response} = RestClient.get_user(rcauth, "userName:mkauffman")
+    # TODO: debug the phoenixdsk-user system role. It's not pulling the user.
+    assert code == :ok
+    assert response.status_code == 200
+
+    {code, response} = RestClient.get_user(rcauth, "userName:nonamehere")
+    assert code == :ok
+    assert response.status_code == 404
+  end
 
 end
